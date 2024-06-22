56fa5b47a95a5309004d370b6d0175282e33958b
SatyawanPanchal <panchal.satyawan@gmail.com>
2024-06-22 06:48:14 +0530

    working commit

--- responsive_navbar/script.js
+++ responsive_navbar/script.js
@@ -5,5 +5,5 @@ const navbarLinks=document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click',()=>{
     navbarLinks.classList.toggle('active')
    
-   window.alert("hello")
+ 
 })
